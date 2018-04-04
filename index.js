6145787e74086be3bdfff3eb4081c48030da2432
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 20:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine (katzDeliLine) {
     return 'The line is currently empty.'
   }
   for (let i=1; i<2; i++){
-    katzDeliLine[i-1] = i + '. ' + katzDeliLine[i] + ' '
+    katzDeliLine[i-1] = i + '. ' + katzDeliLine[i-1] + ' '
   }
   console.log( `The line is currently: ${katzDeliLine}`)
 }
