13e5a0e6174906cc86ef0051421b0e50b334d8e5
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 20:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'The line is currently empty.'
   }
-  for (let i=1; i<2; i++){
+  var length = katzDeliLine.length + 1
+  for (let i=1; i<length; i++){
     katzDeliLine[i-1] = ' '+ i + '. ' + katzDeliLine[i-1]
   }
   console.log( `The line is currently: ${katzDeliLine}`)
