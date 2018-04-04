fcda9db46ccc980bd2bd8b122c09c48850e88f56
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 20:16:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  //console.log (katzDeliLine)
+  katzDeliLine.push(name)
+  //console.log (katzDeliLine)
+  var length = katzDeliLine.length
+  return (`Welcome, ${name}. You are number ${length} in line.`)
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }
+  var name = katzDeliLine[0]
+  katzDeliLine.shift()
+  return `Currently serving ${name}.`
+}
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.'
+  }
+  for (let i=1; i<2; i++){
+    katzDeliLine[i] = i + '. ' + katzDeliLine[i] + ' '
+  }
+  console.log( `The line is currently: ${katzDeliLine}`)
+}
+currentLine (['george', 'anne'])
